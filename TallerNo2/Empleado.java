c0782894eeaf44754f4814be549dd1132cf29c81
daruedar <daruedar@hotmail.com>
2015-09-26 19:06:45 -0500

    Comentarios aclaratorios de codigo
    Añadí algunos comentarios  tipo /**/  para hacer algunas aclaraciones en cuanto al porque de algunas variables y metodos

--- TallerNo2/Empleado.java
+++ TallerNo2/Empleado.java
@@ -2,16 +2,15 @@
 public class Empleado {
 	private String nombre;
 	private String apellido;
-	private double salario;
-	private double salarioAnual;
+	private double salario;/*En Dolares*/
+	private double salarioAnual;/*En Dolares*/
 	
 	public Empleado(String nombre,String apellido, double salario){
 		this.nombre=nombre;
 		this.apellido=apellido;
-		if(salario<0){this.salario=0;}
+		if(salario<0){this.salario=0;}/*Validacion Para posibles salarios negativos*/
 		else{this.salario=salario;}
-		this.salarioAnual=this.salario*12;
-		
+		this.salarioAnual=this.salario*12;/*Salario anual es igual a salario mensual por 12*/
 	}
 
 	public double getSalarioAnual() {
@@ -31,11 +30,9 @@ public class Empleado {
 	}
 	
 	public double  incrementar(double  porcentaje){
-		
-		
+		/**Este metodo  realiza el incremento de  un porcentaje dado para el salario*/
 		this.salarioAnual=this.salarioAnual+(this.salarioAnual* porcentaje)/100;
 		return  salarioAnual;
-		
 	}
 
 }
