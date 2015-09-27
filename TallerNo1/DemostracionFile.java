042f4d87d6ce85c67ce020313f251c46953b08a1
Diego Armando Rueda Robles <daruedar@hotmail.com>
2015-09-26 19:46:34 -0500

    mejorar metodo obtenerArchivoODirectorio
    Poner las respectivas llaves para  cada if, ademas la indexacion es mala,  pruebe y utilice buenas practicas, eso es lo malo de copiar  piezas de codigo  de otros autores a las carreras

--- TallerNo1/DemostracionFile.java
+++ TallerNo1/DemostracionFile.java
@@ -7,33 +7,31 @@ donde se encuantran las palabras para competir en el ahorcado
 */
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.File;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.*;
 
-public class DemostracionFile extends JFrame
-{
+public class DemostracionFile extends JFrame{
 	private File nombreArchivo;
-	public DemostracionFile()
-		{   mimetodo(); }   
-	public void obtenerArchivoODirectorio()
-		{
-			// muestra el cuadro de diálogo de archivos, para que el usuario pueda elegir el archivo a abrir
-			JFileChooser selectorArchivos = new JFileChooser();
-			selectorArchivos.setFileSelectionMode(
-			JFileChooser.FILES_AND_DIRECTORIES );
-			int resultado = selectorArchivos.showOpenDialog( this );
-			// si el usuario hizo clic en el botón Cancelar en el cuadro de diálogo, regresa
-			if ( resultado == JFileChooser.CANCEL_OPTION )
+	public DemostracionFile(){   mimetodo(); }  /*Constructor*/ 
+	public  void  mimetodo(){
+		 obtenerArchivoODirectorio();
+	 } 
+	public void obtenerArchivoODirectorio(){
+		/**Este metodo me despliega una ventana que me permite escoger
+		el archivo en donde estan almacenadas las palabras con las que el jugador
+		va a concursar en el ahorcado
+		*/
+		JFileChooser selectorArchivos = new JFileChooser();
+		selectorArchivos.setFileSelectionMode(
+		JFileChooser.FILES_AND_DIRECTORIES );
+		int resultado = selectorArchivos.showOpenDialog( this );
+		// si el usuario hizo clic en el botón Cancelar en el cuadro de diálogo, regresa
+		if ( resultado == JFileChooser.CANCEL_OPTION )
 			System.exit( 1 );
 			nombreArchivo = selectorArchivos.getSelectedFile(); // obtiene el archivo seleccionado
 			// muestra error si es inválido
+		
 			if ( ( nombreArchivo == null ) || ( nombreArchivo.getName().equals( "" ) ) )
 				{
 					JOptionPane.showMessageDialog( this, "Nombre de archivo inválido",
@@ -44,11 +42,7 @@ public class DemostracionFile extends JFrame
 	public File getNombreArchivo() {
 	return nombreArchivo;
 }
-	public  void  mimetodo()
-   {
-       obtenerArchivoODirectorio();
-       //System.out.println("Te tengo");
-   } 
+
     
    
 } 
